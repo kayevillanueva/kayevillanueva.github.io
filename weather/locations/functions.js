e37baa5d829c66dcf2b4f5f2c414a4766925c2f7
kayevillanueva <kayejanvillanueva@gmail.com>
2020-02-29 22:48:55 -0700

    save changes

---
+++ weather/locations/functions.js
@@ -0,0 +1,64 @@
+'use strict';
+
+// Weather Site JavaScript Functions
+// Listen for the DOM to finish building
+document.addEventListener("DOMContentLoaded", function(){
+    // Variables for Wind Chill function
+    let temp = 51;
+    let speed = 4.8;
+    buildWC(speed,temp);
+    // The Time Indictor function
+    let hour = "5";
+    timeBall(hour);
+    // Implement background weather image
+    let condition = "rain";
+    condition.toLowerCase();
+    changeSummaryImage(condition);
+})
+
+// Displays last modified date
+document.getElementById('date').textContent = document.lastModified;
+
+// Wind chill function
+function buildWC(speed, temp) {
+    let feelTemp = document.getElementById('feelTemp');
+
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+
+    // If chill is greater than temp, return the temp
+    wc = (wc > temp)?temp:wc;
+
+    // Display the windchill
+    console.log(wc);
+    // wc = 'Feels like ' + wc + '&deg;F';
+    feelTemp.innerHTML = wc;
+}
+
+// Time Indicator Function
+function timeBall(hour){
+    // Find all "ball" classes and remove them
+    let x = document.querySelectorAll(".mark");
+    for (let item of x) {
+        console.log(item);
+        item.classList.remove("mark");
+    }
+    
+    // Find all hours that match the parameter and add the "ball" class
+    let hr = document.querySelectorAll(".n"+ hour);
+    for (let item of hr){
+        item.classList.add("mark");
+    }
+}
+
+// display background image
+function changeSummaryImage(condition){
+    let container = document.querySelector("#curWeather")
+    console.log(condition);
+    container.classList.add(condition)
+}
+  
